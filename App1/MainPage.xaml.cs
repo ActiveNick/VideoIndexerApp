e7129fcd15ec1a888c48d9b881ec5d813ae75781
ActiveNick <nick.landry@microsoft.com>
2020-06-25 20:17:21 -0400

    Simple retrieval + WIP on deserialization classes

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -7,6 +8,7 @@ using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Networking.Connectivity;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,9 +25,13 @@ namespace VideoIndexerClient
     public sealed partial class MainPage : Page
     {
         string apiUrl = "https://api.videoindexer.ai";
-        string accountId = "...";
+        string accountId = "fff170b3-e768-443e-a0b2-a19ffd0eb5c7";
         string location = "eastus2"; // replace with the account's location, or with “trial” if this is a trial account
-        string apiKey = "...";
+        string apiKey = "3ac35f4287c44437b92a5329717f31d8";
+
+        // Session objects
+        HttpClient client;
+        string accountAccessToken;
 
         public MainPage()
         {
@@ -39,14 +45,25 @@ namespace VideoIndexerClient
             // create the http client
             var handler = new HttpClientHandler();
             handler.AllowAutoRedirect = false;
-            var client = new HttpClient(handler);
+            client = new HttpClient(handler);
             client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiKey);
 
             // obtain account access token
             var accountAccessTokenRequestResult = client.GetAsync($"{apiUrl}/auth/{location}/Accounts/{accountId}/AccessToken?allowEdit=true").Result;
-            var accountAccessToken = accountAccessTokenRequestResult.Content.ReadAsStringAsync().Result.Replace("\"", "");
+            accountAccessToken = accountAccessTokenRequestResult.Content.ReadAsStringAsync().Result.Replace("\"", "");
 
             client.DefaultRequestHeaders.Remove("Ocp-Apim-Subscription-Key");
+
+            Debug.WriteLine("Video Indexer connection authorized: " + accountAccessToken);
+        }
+
+        private void btnGetVideos_Click(object sender, RoutedEventArgs e)
+        {
+            var searchRequestResult = client.GetAsync($"{apiUrl}/{location}/Accounts/{accountId}/Videos/Search?accessToken={accountAccessToken}").Result;
+            var searchResult = searchRequestResult.Content.ReadAsStringAsync().Result;
+            Debug.WriteLine("");
+            Debug.WriteLine("Search:");
+            Debug.WriteLine(searchResult);
         }
     }
 }
